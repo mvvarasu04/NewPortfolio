30d0142502a91a00c40509bd9c20cdaa1f36ba84
mvvarasu04 <mvvarasu@gmail.com>
2025-11-25 17:19:15 +0530

    Basic setup from portfolio

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <NavLink
+        to="/"
+        className="font-bold text-2xl text-blue-600 hover:text-rose-800"
+        
+      >
+        Veera
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className= {({ isActive  }) => isActive ? "text-blue-600 " : "text-black hover:text-pink-500" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black hover:text-purple-600"}>
+          Projects
+        </NavLink>
+       
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
